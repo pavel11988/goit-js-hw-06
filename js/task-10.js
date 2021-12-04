1cddc0792a51716492a4d641797acba32d03bf06
Pavel <kossoua88@gmail.com>
2021-12-04 03:08:22 +0300

    1-10 ok

--- js/task-10.js
+++ js/task-10.js
@@ -1,39 +1,51 @@
-// const newDiv = `<div>DIVVV</div>`;
+const newDiv = `<div class='newDiv'></div>`;
 
-// const refs = {
-//   inputCount: document.querySelector('#controls > input'),
-//   buttonCreate:document.querySelector('button[data-create]'), 
-//   buttonDestroy:document.querySelector('button[data-destroy]'),
-//   divBox: document.querySelector('#boxes'),
-//   counter: 0,
-// }
-
-
-// console.log(refs.inputCount.value);
+const refs = {
+  inputCount: document.querySelector('#controls > input'),
+  buttonCreate:document.querySelector('button[data-create]'), 
+  buttonDestroy:document.querySelector('button[data-destroy]'),
+  divBox: document.querySelector('#boxes'),
+}
 
 
-// refs.inputCount.addEventListener('input', onInputCount);
-// refs.inputCount.addEventListener('blur', onDivToCreate);
-// refs.buttonCreate.addEventListener('click', onCreateDivs);
-
-// function onInputCount (event) {
-//   if (refs.inputCount.value > 100) {refs.inputCount.value = 100};
-//   if (refs.inputCount.value <= 0)  {refs.inputCount.value = 1}
-//   // console.log(refs.inputCount.value);
-// }
-
-// function onDivToCreate (event) {
-//   newDiv =
-// }
-
-// function onCreateDivs (event) {
-//   const prevSize = 30;
-//   const arr = Array(Number(refs.inputCount.value)).fill(newDiv);
-//   refs.divBox.innerHTML = '';
-//   refs.divBox.insertAdjacentHTML('beforeend', arr.join(''));
-// }
+console.log(refs.inputCount.value);
 
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+refs.inputCount.addEventListener('input', onInputCount);
+refs.buttonCreate.addEventListener('click', createBoxes);
+refs.buttonDestroy.addEventListener('click', destroyBoxes);
+
+function onInputCount (event) {
+  if (refs.inputCount.value > 100) {refs.inputCount.value = 100};
+  if (refs.inputCount.value <= 0)  {refs.inputCount.value = 1}
+}
+
+
+function createBoxes (event) {
+  let prevSize = 30;
+  
+    for (let i = 0; i < refs.inputCount.value; i += 1){
+   
+        refs.divBox.insertAdjacentHTML('beforeend', newDiv);
+        refs.divBox.lastElementChild.style.backgroundColor = getRandomHexColor();
+        if (refs.divBox.children.length === 1)
+        {
+            refs.divBox.lastElementChild.style.width = `${prevSize}px`;
+            refs.divBox.lastElementChild.style.height = `${prevSize}px`;
+        } 
+        else {
+            refs.divBox.lastElementChild.style.width = `${prevSize + 10}px`;
+            refs.divBox.lastElementChild.style.height = `${prevSize + 10}px`;
+            prevSize += 10
+        }
+    }
+}
+
+
+function destroyBoxes (event) {
+    refs.divBox.innerHTML = '';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
